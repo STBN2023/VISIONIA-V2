2e3f5b284f71863e2d143f0133c46966808bd15a
[dyad] <stbn2023@gmail.com>
2025-09-23 10:01:22 +0200

    [dyad] Fix Index.tsx JSX error - wrote 1 file(s) + extra files edited outside of Dyad

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -21,7 +21,7 @@ const Index = () => {
             </Link>
           </div>
           <div className="mt-8">
-            < />
+            <MadeWithDyad />
           </div>
         </GlassPanel>
       </main>
